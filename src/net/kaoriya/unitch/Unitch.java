1bcfbe4881987ff31226828f4d16f6a648bf16a5
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-09-13 09:52:04 +0900

    add UI parts

--- src/net/kaoriya/unitch/Unitch.java
+++ src/net/kaoriya/unitch/Unitch.java
@@ -2,9 +2,13 @@ package net.kaoriya.unitch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Unitch extends Activity
 {
+    public final static String TAG = "Unitch";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -12,4 +16,19 @@ public class Unitch extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    public void onClick_start(View view)
+    {
+        Log.v(TAG, "start");
+        findViewById(R.id.start).setEnabled(false);
+        findViewById(R.id.stop).setEnabled(true);
+    }
+
+    public void onClick_stop(View view)
+    {
+        Log.v(TAG, "stop");
+        findViewById(R.id.stop).setEnabled(false);
+        findViewById(R.id.start).setEnabled(true);
+    }
+
 }
